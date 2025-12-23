f7120ba194fd659e589a1fe78d6944ff529e8e67
Ramazan <mramazantrn28@gmail.com>
2025-12-23 16:37:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -332,6 +332,6 @@ def send_static(path):
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    main()
+
