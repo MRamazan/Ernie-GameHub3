cf502cafde4cf98ea5883c88cc1c774dc5e4a627
Ramazan <mramazantrn28@gmail.com>
2025-12-24 01:02:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -337,10 +337,8 @@ def send_static(path):
 
 
 if __name__ == "__main__":
-    app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 8080))
-)
+    os.makedirs('static', exist_ok=True)
+    app.run(debug=True, port=5000)
 
 
 
